7aa82e2eef5aa807895c6c0e9a938720659dbcff
Nataliia Svystula <natashasvistula@gmail.com>
2022-11-29 12:40:25 +0200

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyles';
+
+import { Statistics } from 'components/Statistics/Statistics';
+import { FeedbackOptions } from 'components/Feedback/FeedbackOptions';
+import { Section } from 'components/Section/Section';
+import { Notification } from 'components/Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = e => {
+    const { good, neutral, bad } = this.state;
+    if (e === 'Good') {
+      this.setState({ good: good + 1 });
+    } else if (e === 'Neutral') {
+      this.setState({ neutral: neutral + 1 });
+    } else if (e === 'Bad') {
+      this.setState({ bad: bad + 1 });
+    }
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total > 0 ? Math.round((100 / total) * good) : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['Good', 'Neutral', 'Bad']}
+            onLeaveFeedback={this.handleFeedback}
+          />{' '}
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() !== 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+        <GlobalStyle />
+      </div>
+    );
+  }
 };
+
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ good, neutral, bad, total, positivePercentage,
+}) => {
+    return (
+        <ul className={css.feedbackList}>
+            <li className={css.feedbackListItem}>Good: {good}</li>
+            <li className={css.feedbackListItem}>Neutral: {neutral}</li>
+            <li className={css.feedbackListItem}>Bad: {bad}</li>
+            <li className={css.feedbackListItem}>Total: {total}</li>
+            <li className={css.feedbackListItem}>
+                Positive feedback: {positivePercentage} %
+            </li>
+        </ul>
+    );
+}
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
